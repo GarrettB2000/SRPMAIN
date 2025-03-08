b5f0a38cac9cf783fc281e5985611ebee7b2604e
GarrettB2000 <gbrown.2521@gmail.com>
2025-03-07 18:15:08 -0600

    9

---
+++ resources/[vehicles]/SRP-Vehicles/stream/[X]/[Garrett]/vc_m4g82adrowidebody/fxmanifest.lua
@@ -0,0 +1,24 @@
+game 'gta5'
+fx_version 'cerulean'
+author 'versus_cars'
+version '1.0'
+ 
+files {
+    'data/**/vehicles.meta',
+    'data/**/carvariations.meta',
+    'data/**/carcols.meta',
+    'data/**/handling.meta',
+    'data/**/vehiclelayouts.meta',
+    'data/**/clip_sets.xml',
+
+}
+
+data_file 'HANDLING_FILE' 'data/**/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/**/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/**/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/vehiclelayouts.meta'
+data_file 'CLIP_SETS_FILE' 'data/**/clip_sets.xml'
+dependency '/assetpacks'
+dependency '/assetpacks'
+dependency '/assetpacks'
